ef05fb638fef9db4442f7385af98359a6bc73242
useusername1 <reknvqvjoq123@gmail.com>
2023-05-19 23:42:17 +0900

    [3주차-큐] 문제5 - 김진주

---
+++ 3주차) 큐/q05/jin123.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubarraySumCircular = function (nums) {
+  let lmax = 0,
+    gmax = nums[0];
+  let lmin = 0,
+    gmin = nums[0];
+  let sum = 0;
+  for (const num of nums) {
+    lmax = Math.max(num, lmax + num);
+    if (gmax < lmax) gmax = lmax;
+    lmin = Math.min(num, lmin + num);
+    if (gmin > lmin) gmin = lmin;
+    sum += num;
+  }
+  console.log(gmax, gmin);
+  return Math.max(gmax, sum - gmin || gmin);
+};
