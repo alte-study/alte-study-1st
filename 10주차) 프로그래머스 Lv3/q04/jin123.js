2eef900dfa24fbf6a32bdb831507a749ef55b61f
useusername1 <reknvqvjoq123@gmail.com>
2023-07-13 22:00:38 +0900

    Update lv3_4_jin123.js

--- 10주차) 프로그래머스 Lv3/q04/jin123.js
+++ 10주차) 프로그래머스 Lv3/q04/jin123.js
@@ -2,30 +2,31 @@ class Table {
   constructor() {
     this.table = new Map();
   }
+  getKey(r, c) {
+    return `${r},${c}`;
+  }
   get(r, c) {
-    let value = this.table.get(r + "," + c);
-    return value ? value.split(",") : null;
+    let value = this.table.get(this.getKey(r, c));
+    return value ? value.split(",").map(Number) : null;
   }
   set(r1, c1, r2, c2) {
-    this.table.set(r1 + "," + c1, r2 + "," + c2);
+    this.table.set(this.getKey(r1, c1), this.getKey(r2, c2));
   }
   has(r, c) {
-    return this.table.has(r + "," + c);
+    return this.table.has(this.getKey(r, c));
   }
   update(r1, c1, r2, c2) {
-    const targetKey = r1 + "," + c1;
-    const targetValue = r2 + "," + c2;
-    let keys = this.table.entries();
-    for (const [key, value] of keys) {
+    const targetKey = this.getKey(r1, c1);
+    const targetValue = this.getKey(r2, c2);
+    for (const [key, value] of this.table) {
       if (value === targetKey) {
         this.table.set(key, targetValue);
       }
     }
   }
   delete(r, c) {
-    const target = r + "," + c;
-    const keys = this.table.entries();
-    for (const [key, value] of keys) {
+    const target = this.getKey(r, c);
+    for (const [key, value] of this.table) {
       if (value === target) {
         this.table.delete(key);
       }
@@ -52,9 +53,8 @@ function solution(commands) {
       case "UPDATE": {
         let [r, c, value] = cmd.slice(1);
         if (cmd.length === 4) {
-          let flag = table.get(r, c);
-          if (flag) [r, c] = flag;
-          board[+r][+c] = value;
+          target = table.get(r, c) || [+r, +c];
+          board[target[0]][target[1]] = value;
         } else {
           updateArr(board, r, c);
         }
@@ -66,47 +66,46 @@ function solution(commands) {
           (r1 === r2 && c1 === c2) ||
           (table.has(r1, c1) &&
             table.has(r2, c2) &&
-            table.get(r1, c1).join("") === table.get(r2, c2).join(""))
+            table.get(r1, c1).join() === table.get(r2, c2).join())
         )
           break;
         if (!table.has(r1, c1)) {
           table.set(r1, c1, r1, c1);
         }
-        const [idx1, idx2] = table.get(r1, c1).map(Number);
+        const [r1_i, c1_i] = table.get(r1, c1);
         if (table.has(r2, c2)) {
-          const [idx3, idx4] = table.get(r2, c2).map(Number);
-          if (!board[idx1][idx2]) {
-            board[idx1][idx2] = board[idx3][idx4];
+          const [r2_i, c2_i] = table.get(r2, c2);
+          if (!board[r1_i][c1_i]) {
+            board[r1_i][c1_i] = board[r2_i][c2_i];
           }
-          board[idx3][idx4] = undefined;
-          table.update(idx3, idx4, idx1, idx2);
+          board[r2_i][c2_i] = undefined;
+          table.update(r2_i, c2_i, r1_i, c1_i);
         } else {
-          if (!board[idx1][idx2]) {
-            board[idx1][idx2] = board[r2][c2];
+          if (!board[r1_i][c1_i]) {
+            board[r1_i][c1_i] = board[r2][c2];
           }
           board[r2][c2] = undefined;
-          table.set(r2, c2, idx1, idx2);
+          table.set(r2, c2, r1_i, c1_i);
         }
         break;
       }
       case "UNMERGE": {
-        let [r, c] = cmd.slice(1);
+        let [r, c] = cmd.slice(1).map(Number);
         const flag = table.get(r, c);
         if (flag) {
           const [x, y] = flag;
           if (!(r === x && c === y)) {
-            board[+r][+c] = board[+x][+y];
-            board[+x][+y] = undefined;
+            board[r][c] = board[x][y];
+            board[x][y] = undefined;
           }
           table.delete(x, y);
         }
         break;
       }
       case "PRINT": {
-        let [r, c] = cmd.slice(1);
-        const flag = table.get(r, c);
-        if (flag) [r, c] = flag;
-        result.push(board[+r][+c] ? board[+r][+c] : "EMPTY");
+        let [r, c] = cmd.slice(1).map(Number);
+        const target = table.get(r, c) || [r, c];
+        result.push(board[target[0]][target[1]] || "EMPTY");
         break;
       }
     }
